72211d5db314d196cacb7d836d0ac9511b504652
Atul Jha <19bcs1752@gmail.com>
2025-05-08 03:50:01 +0530

    mobile grid changes

--- src/pages/brand/[slug].tsx
+++ src/pages/brand/[slug].tsx
@@ -320,7 +320,7 @@ export default function BrandPage() {
                 <ProductSort onSortChange={handleSortChange} />
               </div>
 
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
                 {filteredProducts.map((product) => (
                   <ProductCardFeatured
                     key={product.id}
